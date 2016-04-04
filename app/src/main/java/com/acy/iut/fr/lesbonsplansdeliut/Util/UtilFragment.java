f851e50c3fcf39dea238d50139034e4bb50865ce
Lucas <lucasmesagouiffes@gmail.com>
2016-04-04 08:57:31 +0200

    COMMIT  UtilFragment

---
+++ app/src/main/java/com/acy/iut/fr/lesbonsplansdeliut/Util/UtilFragment.java
@@ -0,0 +1,16 @@
+package com.acy.iut.fr.lesbonsplansdeliut.Util;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+
+import com.acy.iut.fr.lesbonsplansdeliut.Pages.Main;
+
+/**
+ * Created by Lucas on 04/04/2016.
+ */
+public class UtilFragment extends Fragment {
+
+    public Boolean backAction(){
+        return false;
+    }
+}
